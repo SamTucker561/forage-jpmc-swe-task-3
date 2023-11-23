c0b8b02b9c832a7f1ba30fb3e5497aa413db75e6
SamTucker561 <55509480+SamTucker561@users.noreply.github.com>
2023-11-23 22:09:36 +0000

    final

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -17,8 +17,8 @@ export class DataManipulator {
     const price_abc = (serverRespond[0].top_ask.price+serverRespond[0].top_bid.price)/2;
     const price_def = (serverRespond[1].top_ask.price+serverRespond[1].top_bid.price)/2;
     const ratio = price_abc/price_def;
-    const upper_bound = 1 + 0.02;
-    const lower_bound = 1 - 0.02;
+    const upper_bound = 1 + 0.03;
+    const lower_bound = 1 - 0.03;
     const timestamp = serverRespond[0].timestamp > serverRespond[1].timestamp ? serverRespond[0].timestamp:serverRespond[1].timestamp;
     const trigger_alert = (ratio>upper_bound || ratio<lower_bound) ? ratio:undefined;
     return {
