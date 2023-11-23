ef65d8482e78c80c6ce0381a9dce9042a286f3fa
SamTucker561 <55509480+SamTucker561@users.noreply.github.com>
2023-11-23 21:51:44 +0000

    1st

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,34 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc: number,
+  price_def: number,
+  ratio: number,
   timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
 }
 
 
+
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverRespond: ServerRespond[]) {
+    const price_abc = (serverRespond[0].top_ask.price+serverRespond[0].top_bid.price)/2;
+    const price_def = (serverRespond[1].top_ask.price+serverRespond[1].top_bid.price)/2;
+    const ratio = price_abc/price_def;
+    const upper_bound = 1 + 0.02;
+    const lower_bound = 1 - 0.02;
+    const timestamp = serverRespond[0].timestamp > serverRespond[1].timestamp ? serverRespond[0].timestamp:serverRespond[1].timestamp;
+    const trigger_alert = (ratio>upper_bound || ratio<lower_bound) ? ratio:undefined;
+    return {
+      price_abc,
+      price_def,
+      ratio,
+      timestamp,
+      upper_bound,
+      lower_bound,
+      trigger_alert,
+    };
   }
 }
